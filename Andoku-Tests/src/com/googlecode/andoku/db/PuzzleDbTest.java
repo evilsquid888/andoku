663f00956d3d822d3419a292bc9a78d21475717f
markus.wiederkehr@gmail.com <markus.wiederkehr@gmail.com@8b5035ae-46ba-11de-80ae-c1ff141340a0>
2009-10-18 19:06:07 +0000

    added PuzzleDb and unit tests (issue #7)
    git-svn-id: http://andoku.googlecode.com/svn/trunk@75 8b5035ae-46ba-11de-80ae-c1ff141340a0

---
+++ Andoku-Tests/src/com/googlecode/andoku/db/PuzzleDbTest.java
@@ -0,0 +1,218 @@
+/*
+ * Andoku - a sudoku puzzle game for Android.
+ * Copyright (C) 2009  Markus Wiederkehr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.googlecode.andoku.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.database.Cursor;
+import android.database.SQLException;
+import android.test.AndroidTestCase;
+
+import com.googlecode.andoku.db.PuzzleDb;
+
+public class PuzzleDbTest extends AndroidTestCase {
+	private PuzzleDb db;
+
+	@Override
+	protected void setUp() throws Exception {
+		db = new PuzzleDb(getContext());
+		db.resetAll();
+	}
+
+	public void testIsEmpty() throws Exception {
+		assertTrue(loadFolders().isEmpty());
+	}
+
+	public void testCreateFolder() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		assertTrue(folderId1 >= 0);
+
+		Map<Long, String> folders = loadFolders();
+		assertEquals(1, folders.size());
+		assertEquals("f1", folders.get(folderId1));
+
+		long folderId2 = db.createFolder("f2");
+		assertTrue(folderId2 >= 0);
+
+		folders = loadFolders();
+		assertEquals(2, folders.size());
+		assertEquals("f1", folders.get(folderId1));
+		assertEquals("f2", folders.get(folderId2));
+	}
+
+	public void testCreateSubFolder() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder(folderId1, "f2");
+		assertTrue(folderId2 >= 0);
+
+		Map<Long, String> folders = loadFolders();
+		assertEquals(1, folders.size());
+		assertEquals("f1", folders.get(folderId1));
+
+		folders = loadFolders(folderId1);
+		assertEquals(1, folders.size());
+		assertEquals("f2", folders.get(folderId2));
+	}
+
+	public void testFolderNameIsUnique() throws Exception {
+		long folderId1 = db.createFolder("folder");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder(folderId1, "folder");
+		assertTrue(folderId2 >= 0);
+
+		try {
+			db.createFolder("folder");
+			fail();
+		} catch (SQLException expected) {
+		}
+
+		try {
+			db.createFolder(folderId1, "folder");
+			fail();
+		} catch (SQLException expected) {
+		}
+	}
+
+	public void testFolderExistsByName() throws Exception {
+		assertFalse(db.folderExists("folder"));
+
+		long folderId1 = db.createFolder("folder");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder(folderId1, "folder");
+		assertTrue(folderId2 >= 0);
+
+		assertTrue(db.folderExists("folder"));
+
+		assertTrue(db.folderExists(folderId1,
+				"folder"));
+
+		assertFalse(db.folderExists("nosuchfolder"));
+	}
+
+	public void testGetFolderId() throws Exception {
+		assertNull(db.getFolderId("folder"));
+
+		long folderId1 = db.createFolder("folder");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder(folderId1, "folder");
+		assertTrue(folderId2 >= 0);
+
+		assertEquals(new Long(folderId1), db.getFolderId("folder"));
+
+		assertEquals(new Long(folderId2), db.getFolderId(folderId1,
+				"folder"));
+
+		assertNull(db.getFolderId("nosuchfolder"));
+	}
+
+	public void testGetFolderName() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder(folderId1, "f2");
+		assertTrue(folderId2 >= 0);
+
+		assertEquals("f1", db.getFolderName(folderId1));
+		assertEquals("f2", db.getFolderName(folderId2));
+		assertNull(db.getFolderName(folderId2 + 1));
+	}
+
+	public void testGetParentFolderId() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder(folderId1, "f2");
+		assertTrue(folderId2 >= 0);
+
+		assertEquals(new Long(PuzzleDb.ROOT_FOLDER_ID), db
+				.getParentFolderId(folderId1));
+		assertEquals(new Long(folderId1), db.getParentFolderId(folderId2));
+		assertNull(db.getParentFolderId(folderId2 + 1));
+	}
+
+	public void testRenameFolder() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		assertTrue(folderId1 >= 0);
+		assertEquals("f1", db.getFolderName(folderId1));
+		
+		db.renameFolder(folderId1, "newf1");
+		assertEquals("newf1", db.getFolderName(folderId1));
+
+		long folderId2 = db.createFolder(folderId1, "f2");
+		assertTrue(folderId2 >= 0);
+		assertEquals("f2", db.getFolderName(folderId2));
+
+		db.renameFolder(folderId2, "newf2");
+		assertEquals("newf2", db.getFolderName(folderId2));
+	}
+
+	public void testCannotRenameAsExistingFolder() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		assertTrue(folderId1 >= 0);
+
+		long folderId2 = db.createFolder("f2");
+		assertTrue(folderId2 >= 0);
+		
+		try {
+			db.renameFolder(folderId1, "f2");
+			fail();
+		} catch (SQLException e) {
+		}
+	}
+
+	public void testDeleteFolder() throws Exception {
+		long folderId1 = db.createFolder("f1");
+		long folderId2 = db.createFolder(folderId1, "f2");
+		long folderId3 = db.createFolder(folderId1, "f3");
+		long folderId4 = db.createFolder(folderId2, "f4");
+
+		assertTrue(db.folderExists(folderId1));
+		assertTrue(db.folderExists(folderId2));
+		assertTrue(db.folderExists(folderId3));
+		assertTrue(db.folderExists(folderId4));
+		
+		db.deleteFolder(folderId1);
+
+		assertFalse(db.folderExists(folderId1));
+		assertFalse(db.folderExists(folderId2));
+		assertFalse(db.folderExists(folderId3));
+		assertFalse(db.folderExists(folderId4));
+	}
+
+	private Map<Long, String> loadFolders() {
+		return loadFolders(PuzzleDb.ROOT_FOLDER_ID);
+	}
+
+	private Map<Long, String> loadFolders(long parent) {
+		Map<Long, String> folders = new HashMap<Long, String>();
+
+		Cursor cursor = db.getFolders(parent);
+		while (cursor.moveToNext())
+			folders.put(cursor.getLong(0), cursor.getString(1));
+		cursor.close();
+
+		return folders;
+	}
+}
---
+++ Andoku/src/com/googlecode/andoku/db/PuzzleDb.java
@@ -0,0 +1,333 @@
+/*
+ * Andoku - a sudoku puzzle game for Android.
+ * Copyright (C) 2009  Markus Wiederkehr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.googlecode.andoku.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+import com.googlecode.andoku.Constants;
+
+public class PuzzleDb {
+	private static final String TAG = PuzzleDb.class.getName();
+
+	public static final String DATABASE_NAME = "puzzles.db";
+	private static final int DATABASE_VERSION = 1;
+
+	private static final char PATH_SEPARATOR_CHAR = '/';
+
+	private static final String COL_ID = BaseColumns._ID;
+
+	private static final String TABLE_FOLDERS = "folders";
+	public static final String COL_FOLDER_NAME = "name";
+	public static final String COL_FOLDER_PARENT = "parent";
+
+	private static final String TABLE_PUZZLES = "puzzles";
+	private static final String COL_FOLDER = "folder";
+	private static final String COL_NAME = "name";
+	private static final String COL_DIFFICULTY = "difficulty"; // 0-4|-1
+	private static final String COL_SOLUTION = "solution"; //     "35869127496158734217.."
+	private static final String COL_CLUES = "clues"; //           "00010110000000010000.."
+	private static final String COL_AREAS = "areas"; //           "11122223311122222341.."|null
+	private static final String COL_EXTRA_REGIONS = "extra"; //   "H"|"X"|null
+
+	public static final int ROOT_FOLDER_ID = -1;
+
+	private DatabaseHelper openHelper;
+
+	public PuzzleDb(Context context) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "PuzzleDb()");
+
+		openHelper = new DatabaseHelper(context);
+	}
+
+	public void resetAll() {
+		if (Constants.LOG_V)
+			Log.v(TAG, "resetAll()");
+
+		SQLiteDatabase db = openHelper.getWritableDatabase();
+
+		try {
+			db.delete(TABLE_FOLDERS, null, null);
+			db.delete(TABLE_PUZZLES, null, null);
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	public long createFolder(String name) {
+		return createFolder(ROOT_FOLDER_ID, name);
+	}
+
+	public long createFolder(long parentId, String name) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "createFolder(" + parentId + "," + name + ")");
+
+		checkValidFolderName(name);
+
+		SQLiteDatabase db = openHelper.getWritableDatabase();
+
+		try {
+			ContentValues values = new ContentValues();
+			values.put(COL_FOLDER_NAME, name);
+			values.put(COL_FOLDER_PARENT, parentId);
+
+			long insertedRowId = db.insert(TABLE_FOLDERS, null, values);
+			if (insertedRowId == -1)
+				throw new SQLException("Could not create folder " + name);
+
+			return insertedRowId;
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	public boolean folderExists(String name) {
+		return getFolderId(ROOT_FOLDER_ID, name) != null;
+	}
+
+	public boolean folderExists(long parentId, String name) {
+		return getFolderId(parentId, name) != null;
+	}
+
+	public Long getFolderId(String name) {
+		return getFolderId(ROOT_FOLDER_ID, name);
+	}
+
+	public Long getFolderId(long parentId, String name) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "getFolderId(" + parentId + "," + name + ")");
+
+		SQLiteDatabase db = openHelper.getReadableDatabase();
+
+		try {
+			String[] columns = { COL_ID };
+			String selection = COL_FOLDER_NAME + "=? AND " + COL_FOLDER_PARENT + "=?";
+			String[] selectionArgs = { name, String.valueOf(parentId) };
+			Cursor cursor = db.query(TABLE_FOLDERS, columns, selection, selectionArgs, null, null,
+					null);
+			try {
+				if (cursor.moveToNext())
+					return cursor.getLong(0);
+				else
+					return null;
+			}
+			finally {
+				cursor.close();
+			}
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	public Cursor getFolders() {
+		return getFolders(ROOT_FOLDER_ID);
+	}
+
+	public Cursor getFolders(long parentId) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "getFolders(" + parentId + ")");
+
+		SQLiteDatabase db = openHelper.getReadableDatabase();
+
+		return getFolders(db, parentId);
+	}
+
+	private Cursor getFolders(SQLiteDatabase db, long parentId) {
+		String selection = COL_FOLDER_PARENT + "=?";
+		String[] selectionArgs = { String.valueOf(parentId) };
+		final String orderBy = COL_FOLDER_NAME + " asc";
+		return db.query(TABLE_FOLDERS, null, selection, selectionArgs, null, null, orderBy);
+	}
+
+	public boolean folderExists(long folderId) {
+		return getFolderName(folderId) != null;
+	}
+
+	public String getFolderName(long folderId) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "getFolderName(" + folderId + ")");
+
+		SQLiteDatabase db = openHelper.getReadableDatabase();
+
+		try {
+			String[] columns = { COL_FOLDER_NAME };
+			String selection = COL_ID + "=?";
+			String[] selectionArgs = { String.valueOf(folderId) };
+			Cursor cursor = db.query(TABLE_FOLDERS, columns, selection, selectionArgs, null, null,
+					null);
+
+			try {
+				if (cursor.moveToFirst())
+					return cursor.getString(0);
+				else
+					return null;
+			}
+			finally {
+				cursor.close();
+			}
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	public Long getParentFolderId(long folderId) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "getParentFolderId(" + folderId + ")");
+
+		SQLiteDatabase db = openHelper.getReadableDatabase();
+
+		try {
+			String[] columns = { COL_FOLDER_PARENT };
+			String selection = COL_ID + "=?";
+			String[] selectionArgs = { String.valueOf(folderId) };
+			Cursor cursor = db.query(TABLE_FOLDERS, columns, selection, selectionArgs, null, null,
+					null);
+
+			try {
+				if (cursor.moveToFirst())
+					return cursor.getLong(0);
+				else
+					return null;
+			}
+			finally {
+				cursor.close();
+			}
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	public void renameFolder(long folderId, String newName) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "renameFolder(" + folderId + "," + newName + ")");
+
+		checkValidFolderName(newName);
+
+		SQLiteDatabase db = openHelper.getWritableDatabase();
+		try {
+			ContentValues values = new ContentValues();
+			values.put(COL_FOLDER_NAME, newName);
+
+			String whereClause = COL_ID + "=?";
+			String[] whereArgs = { String.valueOf(folderId) };
+			int rows = db.update(TABLE_FOLDERS, values, whereClause, whereArgs);
+			if (rows != 1)
+				throw new SQLException("Could not rename folder " + folderId + " in " + newName);
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	public void deleteFolder(long folderId) {
+		if (Constants.LOG_V)
+			Log.v(TAG, "deleteFolder(" + folderId + ")");
+
+		SQLiteDatabase db = openHelper.getWritableDatabase();
+		try {
+			deleteFolder(db, folderId);
+		}
+		finally {
+			db.close();
+		}
+	}
+
+	private void deleteFolder(SQLiteDatabase db, long folderId) {
+		recursivelyDeleteSubFolders(db, folderId);
+
+		deletePuzzles(db, folderId);
+
+		deleteFolder0(db, folderId);
+	}
+
+	private void recursivelyDeleteSubFolders(SQLiteDatabase db, long folderId) {
+		Cursor cursor = getFolders(db, folderId);
+		try {
+			while (cursor.moveToNext()) {
+				long subFolderId = cursor.getLong(0);
+				deleteFolder(db, subFolderId);
+			}
+		}
+		finally {
+			cursor.close();
+		}
+	}
+
+	private void deletePuzzles(SQLiteDatabase db, long folderId) {
+		String whereClause = COL_FOLDER + "=?";
+		String[] whereArgs = { String.valueOf(folderId) };
+		db.delete(TABLE_PUZZLES, whereClause, whereArgs);
+	}
+
+	private void deleteFolder0(SQLiteDatabase db, long folderId) {
+		String whereClause = COL_ID + "=?";
+		String[] whereArgs = { String.valueOf(folderId) };
+		int rows = db.delete(TABLE_FOLDERS, whereClause, whereArgs);
+		if (rows != 1)
+			throw new SQLException("Could not delete folder " + folderId);
+	}
+
+	private void checkValidFolderName(String folderName) {
+		if (folderName.indexOf(PATH_SEPARATOR_CHAR) != -1)
+			throw new IllegalArgumentException();
+	}
+
+	/**
+	 * This class helps open, create, and upgrade the database file.
+	 */
+	private static class DatabaseHelper extends SQLiteOpenHelper {
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL("CREATE TABLE " + TABLE_FOLDERS + " (" + COL_ID + " INTEGER PRIMARY KEY,"
+					+ COL_FOLDER_NAME + " TEXT, " + COL_FOLDER_PARENT + " INTEGER, UNIQUE ("
+					+ COL_FOLDER_NAME + ", " + COL_FOLDER_PARENT + "));");
+
+			db.execSQL("CREATE TABLE " + TABLE_PUZZLES + " (" + COL_ID + " INTEGER PRIMARY KEY,"
+					+ COL_FOLDER + " INTEGER," + COL_NAME + " TEXT, " + COL_DIFFICULTY + " INTEGER, "
+					+ COL_SOLUTION + " TEXT, " + COL_CLUES + " TEXT, " + COL_AREAS + " TEXT, "
+					+ COL_EXTRA_REGIONS + " TEXT);");
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.i(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion
+					+ "; which will destroy all old data!");
+
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_FOLDERS);
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_PUZZLES);
+			onCreate(db);
+		}
+	}
+}
