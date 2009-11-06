f472a43d0097c6c1bc4fe0cf9a5f9cac49b6f68e
markus.wiederkehr@gmail.com <markus.wiederkehr@gmail.com@8b5035ae-46ba-11de-80ae-c1ff141340a0>
2009-11-06 21:23:07 +0000

    create Solution from a solved Puzzle
    git-svn-id: http://andoku.googlecode.com/svn/trunk@147 8b5035ae-46ba-11de-80ae-c1ff141340a0

--- Andoku/src/com/googlecode/andoku/model/Solution.java
+++ Andoku/src/com/googlecode/andoku/model/Solution.java
@@ -25,6 +25,21 @@ public class Solution {
 		this.solution = solution;
 	}
 
+	public Solution(Puzzle puzzle) {
+		if (!puzzle.isSolved())
+			throw new IllegalArgumentException();
+
+		final int size = puzzle.getSize();
+		this.solution = new int[size][size];
+
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				int value = puzzle.getValue(row, col);
+				solution[row][col] = value;
+			}
+		}
+	}
+
 	public int getValue(int row, int col) {
 		return solution[row][col];
 	}
