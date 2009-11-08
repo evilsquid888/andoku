3a5fb9005331f6889433d2b0b01e738d9527ddb1
markus.wiederkehr@gmail.com <markus.wiederkehr@gmail.com@8b5035ae-46ba-11de-80ae-c1ff141340a0>
2009-11-08 19:34:29 +0000

    undo changes committed in revision 154; probably not the way to go
    git-svn-id: http://andoku.googlecode.com/svn/trunk@155 8b5035ae-46ba-11de-80ae-c1ff141340a0

--- Andoku/src/com/googlecode/andoku/Constants.java
+++ Andoku/src/com/googlecode/andoku/Constants.java
@@ -31,8 +31,6 @@ public class Constants {
 
 	public static final String EXTRA_FOLDER_ID = PREFIX + "folderId";
 
-	public static final String EXTRA_PUZZLES = PREFIX + "puzzles";
-
 	public static final String IMPORTED_PUZZLES_FOLDER = "Imported Puzzles";
 
 	public static final boolean LOG_V = false;
