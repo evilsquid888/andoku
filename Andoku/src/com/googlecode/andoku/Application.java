4a76364b7f7eaf5f4a901bc6510f1ee83cc1546b
markus.wiederkehr@gmail.com <markus.wiederkehr@gmail.com@8b5035ae-46ba-11de-80ae-c1ff141340a0>
2010-05-02 20:50:25 +0000

    Compute a default value for the fullscreen setting depending on the aspect ratio of the screen
    git-svn-id: http://andoku.googlecode.com/svn/trunk@323 8b5035ae-46ba-11de-80ae-c1ff141340a0

--- Andoku/src/com/googlecode/andoku/Application.java
+++ Andoku/src/com/googlecode/andoku/Application.java
@@ -20,13 +20,41 @@
 
 package com.googlecode.andoku;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
 
 public class Application extends android.app.Application {
+	private static final String TAG = Application.class.getName();
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
-		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		if (preferences.contains(Settings.KEY_FULLSCREEN_MODE)) {
+			Log.d(TAG, "Fullscreen mode has already been set");
+		}
+		else {
+			Log.d(TAG, "Fullscreen mode undecided");
+			Editor editor = preferences.edit();
+			editor.putBoolean(Settings.KEY_FULLSCREEN_MODE, isDefaultFullscreenMode());
+			editor.commit();
+		}
+
+		PreferenceManager.setDefaultValues(this, R.xml.settings, true);
+	}
+
+	private boolean isDefaultFullscreenMode() {
+		DisplayMetrics metrics = getResources().getDisplayMetrics();
+		float aspect = (float) metrics.widthPixels / metrics.heightPixels;
+		boolean fullscreen = aspect >= 320f / 480;
+
+		Log.d(TAG, "Width: " + metrics.widthPixels + ", height: " + metrics.heightPixels
+				+ ", aspect: " + aspect + ", fullscreen: " + fullscreen);
+
+		return fullscreen;
 	}
 }
